4f35b5c38c6d645627f3f3b7e4f3bcbcfbc92151
muhammaddin <muhammaddinabdumutalipov@gmail.com>
2020-06-04 11:52:14 +0500

    abbacda

--- frontend/views/site/tuzilma.php
+++ frontend/views/site/tuzilma.php
@@ -3,145 +3,32 @@
 						<!-- BEGIN .panel -->
 						<div class="panel">
 							<div class="p-title">
-								<h2>Blog Style #2</h2>
+								<h2>Тузилма</h2>
 							</div>
 							<div class="blog-list style-2">
 								
 								<?php foreach ($tuzilma as $blog) :?>
 								<div class="item">
 									<div class="item-header">
-										<a href="post.html"><img src="<?=Yii::getAlias('@web')?>/admin/uploads/structure/<?=$blog->image ?>" alt="" class="item-photo" /></a>
-										<h3><a href="post.html"><?=$blog->name ?> </a></h3>
+										<center>
+											<a href="post.html"><img src="<?=Yii::getAlias('@web')?>/admin/uploads/structure/<?=$blog->image ?>" alt="" class="item-photo" style="width: 200px; height: 250px; "/></a>
+										</center>
+																				
+										
 									</div>
-									<div class="item-content">
-										<p><?=$blog->prof ?></p>
+									<div >
+										<h3 style="font-size:20px;font-weight: bold; font-family: Times New Roman"><a href="#" ><?=$blog->name ?> </a></h3>
+									</div>
+									<div class="item-content" >
+										<p style="font-size:20px;font-weight: bold; font-family: Times New Roman"><?=$blog->prof ?></p>
 									</div>
 									<div class="item-footer">
-										<a href="blog.html" class="left" style="color: #c32929;"><strong>Design</strong></a>
-										<span class="right">
-											<?=$blog->phone	 ?>
-										</span>
+										<p style="font-weight: bold; font-family: Times New Roman; font-size:14px;"><?=$blog->phone?></p>
+										
 									</div>
 								</div>
 								<?php endforeach; ?>
-								<div class="item light">
-									<div class="item-header">
-										<a href="post.html"><img src="<?=Yii::getAlias('@web')?>/foto/images/photos/image-15.jpg" alt="" class="item-photo" /></a>
-										<h3><a href="post.html">BMW E60 Test Drive from TOP GEAR</a></h3>
-									</div>
-									<div class="item-content">
-										<p>Sotheby’s in London is getting ready for another seasonal display of wealth, as some of world’s</p>
-									</div>
-									<div class="item-footer">
-										<a href="blog.html" class="left" style="color: #648126;"><strong>Electronics</strong></a>
-										<span class="right">
-											<a href="blog.html"><i class="fa fa-clock-o"></i> 4 Jan, 2014</a>
-											<a href="blog.html"><i class="fa fa-comment"></i> 3</a>
-										</span>
-									</div>
-								</div>
 								
-								<div class="item">
-									<div class="item-header">
-										<a href="post.html"><img src="<?=Yii::getAlias('@web')?>/foto/images/photos/image-49.jpg" alt="" class="item-photo" /></a>
-										<h3><a href="post.html">BMW E60 Test Drive from TOP GEAR</a></h3>
-									</div>
-									<div class="item-content">
-										<p>Sotheby’s in London is getting ready for another seasonal display of wealth, as some of world’s</p>
-									</div>
-									<div class="item-footer">
-										<a href="blog.html" class="left" style="color: #c32929;"><strong>Design</strong></a>
-										<span class="right">
-											<a href="blog.html"><i class="fa fa-clock-o"></i> 4 Jan, 2014</a>
-											<a href="blog.html"><i class="fa fa-comment"></i> 3</a>
-										</span>
-									</div>
-								</div>
-								
-								<div class="item">
-									<div class="item-header">
-										<a href="post.html"><img src="<?=Yii::getAlias('@web')?>/foto/images/photos/image-50.jpg" alt="" class="item-photo" /></a>
-										<h3><a href="post.html">BMW E60 Test Drive from TOP GEAR</a></h3>
-									</div>
-									<div class="item-content">
-										<p>Sotheby’s in London is getting ready for another seasonal display of wealth, as some of world’s</p>
-									</div>
-									<div class="item-footer">
-										<a href="blog.html" class="left" style="color: #c32929;"><strong>Design</strong></a>
-										<span class="right">
-											<a href="blog.html"><i class="fa fa-clock-o"></i> 4 Jan, 2014</a>
-											<a href="blog.html"><i class="fa fa-comment"></i> 3</a>
-										</span>
-									</div>
-								</div>
-								
-								<div class="item">
-									<div class="item-header">
-										<a href="post.html"><img src="<?=Yii::getAlias('@web')?>/foto/images/photos/image-16.jpg" alt="" class="item-photo" /></a>
-										<h3><a href="post.html">BMW E60 Test Drive from TOP GEAR</a></h3>
-									</div>
-									<div class="item-content">
-										<p>Sotheby’s in London is getting ready for another seasonal display of wealth, as some of world’s</p>
-									</div>
-									<div class="item-footer">
-										<a href="blog.html" class="left" style="color: #c32929;"><strong>Design</strong></a>
-										<span class="right">
-											<a href="blog.html"><i class="fa fa-clock-o"></i> 4 Jan, 2014</a>
-											<a href="blog.html"><i class="fa fa-comment"></i> 3</a>
-										</span>
-									</div>
-								</div>
-								
-								<div class="item light">
-									<div class="item-header">
-										<a href="post.html"><img src="<?=Yii::getAlias('@web')?>/foto/images/photos/image-15.jpg" alt="" class="item-photo" /></a>
-										<h3><a href="post.html">BMW E60 Test Drive from TOP GEAR</a></h3>
-									</div>
-									<div class="item-content">
-										<p>Sotheby’s in London is getting ready for another seasonal display of wealth, as some of world’s</p>
-									</div>
-									<div class="item-footer">
-										<a href="blog.html" class="left" style="color: #648126;"><strong>Electronics</strong></a>
-										<span class="right">
-											<a href="blog.html"><i class="fa fa-clock-o"></i> 4 Jan, 2014</a>
-											<a href="blog.html"><i class="fa fa-comment"></i> 3</a>
-										</span>
-									</div>
-								</div>
-								
-								<div class="item">
-									<div class="item-header">
-										<a href="post.html"><img src="<?=Yii::getAlias('@web')?>/foto/images/photos/image-49.jpg" alt="" class="item-photo" /></a>
-										<h3><a href="post.html">BMW E60 Test Drive from TOP GEAR</a></h3>
-									</div>
-									<div class="item-content">
-										<p>Sotheby’s in London is getting ready for another seasonal display of wealth, as some of world’s</p>
-									</div>
-									<div class="item-footer">
-										<a href="blog.html" class="left" style="color: #c32929;"><strong>Design</strong></a>
-										<span class="right">
-											<a href="blog.html"><i class="fa fa-clock-o"></i> 4 Jan, 2014</a>
-											<a href="blog.html"><i class="fa fa-comment"></i> 3</a>
-										</span>
-									</div>
-								</div>
-								
-								<div class="item">
-									<div class="item-header">
-										<a href="post.html"><img src="<?=Yii::getAlias('@web')?>/foto/images/photos/image-50.jpg" alt="" class="item-photo" /></a>
-										<h3><a href="post.html">BMW E60 Test Drive from TOP GEAR</a></h3>
-									</div>
-									<div class="item-content">
-										<p>Sotheby’s in London is getting ready for another seasonal display of wealth, as some of world’s</p>
-									</div>
-									<div class="item-footer">
-										<a href="blog.html" class="left" style="color: #c32929;"><strong>Design</strong></a>
-										<span class="right">
-											<a href="blog.html"><i class="fa fa-clock-o"></i> 4 Jan, 2014</a>
-											<a href="blog.html"><i class="fa fa-comment"></i> 3</a>
-										</span>
-									</div>
-								</div>
 
 							</div>
 						<!-- END .panel -->
