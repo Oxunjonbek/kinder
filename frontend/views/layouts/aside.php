3704e55636aeefc843e68cc4db2e3bbd6fe4b839
polatalibek <polatmuhammadumar@mail.ru>
2020-06-04 15:15:06 +0500

    aside dagi xatolik

--- frontend/views/layouts/aside.php
+++ frontend/views/layouts/aside.php
@@ -33,7 +33,7 @@ $rows = Yii::$app->db->createCommand('SELECT * FROM `tadbir` RIGHT OUTER JOIN `t
                         <a href="post.html"><img src="<?=Yii::getAlias('@web')?>/foto/images/photos/image-1.jpg" alt="" class="item-photo" /></a>
                         <div class="item-content">
                             <h3><a href="post.html">
-                                <?php $val = mb_substr($value['title'],0,50); echo $val?>
+                                <?php $val = mb_substr($value['title'],0,19); echo $val?>
                                         </a></h3>
                                             <!-- <span>4 Jan, 2014</span>
                                                 <a href="post.html#comments"><span>3 Comments</span></a> -->
