6d8a7af67e8139f92ba6183a1846f023b43438e3
muhammaddin <muhammaddinabdumutalipov@gmail.com>
2020-06-12 14:21:25 +0500

    aa

--- frontend/views/site/mttlar.php
+++ frontend/views/site/mttlar.php
@@ -7,16 +7,35 @@ use yii\helpers\Url; ?>
 		<div class="p-title">
 			<h2>МТТлар</h2>
 		</div>
-		<div class="blog-list style-1">
+		<div class="blog-list style-1" >
 
 			<?php foreach ($mttlar as $key => $value) :?>
-				<div class="item">
+				
+					
+				
+				<div class="item" class="col-sm" style="float: left; margin-right: 18px;">
 
-					<h3><?=$value->name?></h3>
-					<p><?=$value->prof?></p>
-					<p><?=$value->phone?></p>
-					<p><?=$value->address?></p>
+					<h3 style="text-indent: 60px;"><?=$value->name?></h3>
+					<p style="color: #339966; font-size: 18px;">
+						<span style="color: #008000;font-size: 18px; font-weight: bold;">	 Mудира:
+						</span>
+						<?=$value->prof?>
+					</p>
+					<p style="color: #339966; font-size: 18px;">
+						<span style="color: #008000;font-size: 18px; font-weight: bold;">Телефон:
+						</span>
+						<?=$value->phone?>
+							
+					</p>
+					<p style="color: #339966; font-size: 18px;">
+						<span style="color: #008000;font-size: 18px; font-weight: bold;">Манзил:
+						</span>
+						<?=$value->address?>
+							
+					</p>
 				</div>
+
+				
 			<?php endforeach; ?>
 		</div>
 		<!-- END .panel -->
